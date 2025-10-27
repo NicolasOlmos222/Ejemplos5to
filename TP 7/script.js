c99777ee347710f183b6433a59a7789047f1c15d
Nicolás Olmos <80790398+NicolasOlmos222@users.noreply.github.com>
2025-10-27 09:19:07 -0300

    uNDER
    uNDER

---
+++ TP 7/script.js
@@ -0,0 +1,133 @@
+class Estudiante{
+    constructor(nombre, apellido, curso, nota1, nota2, nota3){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.curso  = curso
+        this.nota1 = nota1
+        this.nota2 = nota2
+        this.nota3 = nota3
+    }
+
+    getNombre(){
+        return this.nombre
+    }
+
+    getApellido(){
+        return this.apellido
+    }
+
+    getCurso(){
+        return this.curso
+    }
+
+    getPromedio(){
+        var promedio=(this.nota1+this.nota2+this.nota3)/3
+        return promedio
+    }
+
+    getMejorNota(){
+        if (this.nota1>this.nota2 && this.nota1>this.nota3){
+            return this.nota1
+        }
+
+        else if (this.nota2>this.nota1 && this.nota2>this.nota3){
+            return this.nota2
+        }
+
+        else {
+            return this.nota3
+        }
+    }
+
+
+    getPeorNota(){
+        if (this.nota1<this.nota2 && this.nota1<this.nota3){
+            return this.nota1
+        }
+
+        else if (this.nota2<this.nota1 && this.nota2<this.nota3){
+            return this.nota2
+        }
+
+        else {
+            return this.nota3
+        }
+    }
+
+}
+
+
+class Escuela {
+    constructor(){
+        this.ListaEstudiantes = []
+    }
+
+    agregarEstudiante(estudiante){
+        this.ListaEstudiantes.push(estudiante)
+    }
+
+    getEstudiantes(){
+        return this.ListaEstudiantes
+    }
+
+    getEstudiantePorCurso(curso){
+        var lista = []
+        for (var i = 0; i < this.ListaEstudiantes.length; i ++){
+            var estudiante = this.ListaEstudiantes[i]
+            if (estudiante.getCurso() == curso){
+                lista.push(estudiante)
+            } 
+        }
+
+        return lista
+    }
+
+
+    getEstudiantePorPromedio(promedio){
+        var lista = []
+        for (var i = 0; i < this.ListaEstudiantes.length; i ++){
+            var estudiante = this.ListaEstudiantes[i]
+            if (estudiante.getPromedio() >= promedio){
+                lista.push(estudiante)
+            } 
+        }
+
+        return lista
+    }
+
+    getPeorNota(){
+        var peorEstu = this.ListaEstudiantes[0]
+        for (var i = 0; i < this.ListaEstudiantes.length; i ++){
+            var estudiante = this.ListaEstudiantes[i]
+            if (estudiante.getPeorNota() < peorEstu.getPeorNota()){
+                peorEstu=estudiante
+            }
+        }
+        return peorEstu
+    }
+
+    getMejorNota(){
+        var MejorEstu = this.ListaEstudiantes[0]
+        for (var i = 0; i < this.ListaEstudiantes.length; i ++){
+            var estudiante = this.ListaEstudiantes[i]
+            if (estudiante.getMejorNota() > MejorEstu.getMejorNota()){
+                MejorEstu=estudiante
+            }
+        }
+        return MejorEstu
+    }
+
+    getPromedioGeneral(){
+        var suma = 0
+        for (var i = 0; i < this.ListaEstudiantes.length; i ++){
+            var estudiante = this.ListaEstudiantes[i]
+            suma += estudiante.getPromedio() 
+        }   
+        var promedioGeneral = suma/this.ListaEstudiantes.length
+        return promedioGeneral
+    }
+}
+
+var escuela = new Escuela()
+
+//Sharau al under y a mis viejos
